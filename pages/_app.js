d7048884b30856f681d6bcd7261b797ba2a0f446
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-04-28 18:40:51 -0400

    Added home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../assets/theme';
-import Layout from '../components/layout';
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../assets/theme";
+import Layout from "../components/layout";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -22,9 +22,13 @@ export default function MyApp(props) {
       <Head>
         <title>My page</title>
         <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width'
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+        </style>
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
